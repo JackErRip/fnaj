a95d088b82ad946380b879d7eb3854856d97983d
John Price <136085120+JackErRip@users.noreply.github.com>
2023-11-17 14:35:15 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import OneNightAtJoseph from './OneNightAtJoseph.rar';
+
 
 function App() {
   return (
@@ -7,15 +9,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Play Fnaj
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="OneNightAtJoseph"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h3>Download Latest Five Night's At Joseph</h3>
         </a>
       </header>
     </div>
