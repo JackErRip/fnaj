121655a962d43c953a17491fc679c878a0d8cd8d
John Price <136085120+JackErRip@users.noreply.github.com>
2023-11-17 14:41:58 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import OneNightAtJoseph from './JosephRIGGED.fbx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Play Fnaj
+        </p>
+        <a
+          className="App-link"
+          href="OneNightAtJoseph"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h3>Download Latest Five Night's At Joseph</h3>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
