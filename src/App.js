297aef1e751dce4bbd353e858b8e8dcd7f69868c
John Price <136085120+JackErRip@users.noreply.github.com>
2023-11-18 20:39:12 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <a
           className="App-link"
           href="https://drive.google.com/u/2/uc?id=1xt_FM9JO5WCtFkGzlpE5OV2jqnZnSJb9&export=download"
-          target="_blank"
+          target="_blank" 
           rel="noopener noreferrer"
         >
           <h3>Download Latest Five Night's At Joseph</h3>
